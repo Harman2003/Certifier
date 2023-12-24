14d764d541b3c6ece0c6235ad9ef230678cd5d85
Harman Singh <singhharmandhindsa@gmail.com>
2023-12-24 19:04:11 +0530

    cors error

--- server/config/corsOptions.js
+++ server/config/corsOptions.js
@@ -1,5 +1,5 @@
 const corsOptions = {
-  origin: ["http://localhost:5173", "https://certifier.vercel.app/"],
+  origin: ["http://localhost:5173", "https://certifier.vercel.app"],
   credentials: true,
 };
 
